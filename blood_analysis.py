524976eba6cb60e15461603a4b9403198fa5d5d2
Canyon Duncan <canyon.duncan@duke.edu>
2020-05-20 10:56:42 -0400

    added new function for LDL analysis

--- blood_analysis.py
+++ blood_analysis.py
@@ -1,8 +1,22 @@
 #blood_analysis.py
 
+def HDL_analysis(HDL_result):
+    if HDL_result >= 60:
+        return "Good"
+    elif 40 <= HDL_result < 60:
+        return "Borderline"
+    else:
+        return "Bad"
+
 def HDL_interface():
-    print("HDL")    
-    pass
+    print("HDL Interface")
+    print("Please input the result in the following format:")
+    print("  HDL=## where ## is the numeric result")
+    HDL_input = input("Result: ")
+    HDL_list = HDL_input.split("=")
+    HDL_val = HDL_list[1]
+    HDL_status = HDL_analysis(int(HDL_val))
+    print(HDL_status)
 
 def interface():
     print("My Blood Analysis Calculator")
